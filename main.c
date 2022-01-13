bf0406a0098ae825571138c98f6ba79a017dad62
meritissimo1 <marcelomoreira1905@gmail.com>
2022-01-13 19:11:32 -0300

    colocando pixel na tela

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <mlx.h>
 
+
+
+
 typedef struct	s_data {
 	void	*img;
 	char	*addr;
@@ -8,13 +11,22 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-int my_mlx_pixel_put();
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
