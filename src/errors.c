5d9d07ada8b61cda973b645e72897101a145d9dc
meritissimo1 <marcelomoreira1905@gmail.com>
2022-02-20 10:51:02 -0300

    fixing check initial erros

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: marcrodr < marcrodr@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 16:49:37 by marcrodr          #+#    #+#             */
-/*   Updated: 2022/02/20 10:39:59 by marcrodr         ###   ########.fr       */
+/*   Updated: 2022/02/20 10:49:18 by marcrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@ t_fdf_params	*check_initial_errors(t_fdf_params *fdf, int argc, char **argv)
 		exit_program(fdf, 1);
 	}
 	else
+	{
 		if (ft_strncmp(&(argv[1][ft_strlen(argv[1]) - 4]), ".fdf", 4))
 			exit_program(fdf, 1);
 		fdf->map.path = ft_strdup(argv[1]);
+	}
 	if (!fdf->map.path)
 		exit_program(fdf, 1);
 	return (fdf);
